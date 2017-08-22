5e0745f4d44c26bc82b9c897d297e5eda4359909
Mohseni <mohseni676@gmail.com>
2017-08-22 07:34:09 +0430

    fgsdfg

---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+namespace PazhDataCollect
+{
+    public partial class mainFrm : Form
+    {
+        Utility UT = new Utility();
+        SqlConnectionStringBuilder RemoteCN = new SqlConnectionStringBuilder();
+        SqlConnectionStringBuilder LocalCN = new SqlConnectionStringBuilder();
+        public mainFrm()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (UT.CheckServerAvailablity(txtRserver.Text.Trim())==true)
+                {
+                    //MessageBox.Show("ارتباط با سرور بر قرار است.");
+                    cbDBList.Items.Clear();
+                    List<string> DBs = new List<string>();
+                    DBs = UT.FN_GetDBList(txtRserver.Text.Trim(),  txtDBUser.Text.Trim(), txtDBPassword.Text);
+                    cbDBList.Items.AddRange(DBs.ToArray());
+                    MessageBox.Show("بانک اطلاعاتی مد نظر را از لیست انتخاب نمایید.");
+                    panel3.Enabled = true;
+                    panel2.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("خطا: ارتباط با سرور بر قرار نمی باشد، آدرس و پورت را بررسی کنید.");
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("خطا: " + er.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder cn = new SqlConnectionStringBuilder();
+            cn.DataSource = txtRserver.Text.Trim();
+            cn.UserID = txtDBUser.Text.Trim();
+            cn.Password = txtDBPassword.Text;
+            cn.InitialCatalog = cbDBList.Text.Trim();
+            
+            Properties.Settings.Default.RemoteCN = cn.ConnectionString.ToString();
+            Properties.Settings.Default.Save();
+            MessageBox.Show("تنظیمات مربوط به سرور راه دور ذخیره شد.");
+        }
+
+        private void mainFrm_Load(object sender, EventArgs e)
+        {
+            
+            if (Properties.Settings.Default.RemoteCN != "")
+            {
+                RemoteCN.ConnectionString = Properties.Settings.Default.RemoteCN;
+                txtRserver.Text = RemoteCN.DataSource.ToString();
+                txtDBUser.Text = RemoteCN.UserID.ToString();
+                txtDBPassword.Text = RemoteCN.Password.ToString();
+                cbDBList.Text = RemoteCN.InitialCatalog.ToString();
+            }
+            if (Properties.Settings.Default.LocalCN != "")
+            {
+                LocalCN.ConnectionString = Properties.Settings.Default.LocalCN;
+                txtLServer.Text = LocalCN.DataSource.ToString();
+                txtLDBUser.Text = LocalCN.UserID.ToString();
+                txtLPassword.Text = LocalCN.Password.ToString();
+                cbLDBname.Text = LocalCN.InitialCatalog.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (UT.CheckServerAvailablity(txtLServer.Text.Trim()) == true)
+                {
+                    //MessageBox.Show("ارتباط با سرور بر قرار است.");
+                    cbLDBname.Items.Clear();
+                    List<string> DBs = new List<string>();
+                    DBs = UT.FN_GetDBList(txtLServer.Text.Trim(), txtLDBUser.Text.Trim(), txtLPassword.Text);
+                    cbLDBname.Items.AddRange(DBs.ToArray());
+                    MessageBox.Show("بانک اطلاعاتی مد نظر را از لیست انتخاب نمایید.");
+                    panel4.Enabled = true;
+                    panel5.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("خطا: ارتباط با سرور بر قرار نمی باشد، آدرس و پورت را بررسی کنید.");
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("خطا: " + er.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder cn = new SqlConnectionStringBuilder();
+            cn.DataSource = txtLServer.Text.Trim();
+            cn.UserID = txtLDBUser.Text.Trim();
+            cn.Password = txtLPassword.Text;
+            cn.InitialCatalog = cbLDBname.Text.Trim();
+
+            Properties.Settings.Default.LocalCN = cn.ConnectionString.ToString();
+            Properties.Settings.Default.Save();
+            MessageBox.Show("تنظیمات مربوط به سرور فروشگاه ذخیره شد.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DataSchemeFrm Dform = new DataSchemeFrm();
+            Dform.ShowDialog();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            panel6.Enabled = true;
+            using (SqlConnection LCN = new SqlConnection(Properties.Settings.Default.LocalCN))
+            {
+                LCN.Open();
+                int DaysBefore = Properties.Settings.Default.DaysBefore;
+                string ShopName = Properties.Settings.Default.ShopName;
+                string ShopID = Properties.Settings.Default.ShopID;
+                string DateField = Properties.Settings.Default.DateField;
+                string Cdate = UT.FN_FormatDate(DateTime.Now.AddDays(DaysBefore * -1), true, true);
+                using (SqlDataAdapter DT = new SqlDataAdapter(Properties.Settings.Default.LocalSQL + " Where " + DateField + ">'" + Cdate + "'", LCN))
+                {
+                    DataTable TB = new DataTable();
+                    DT.Fill(TB);
+                    dgLocal.DataSource = TB;
+                }
+            }
+            using (SqlConnection LCN = new SqlConnection(Properties.Settings.Default.RemoteCN))
+            {
+                LCN.Open();
+                using (SqlDataAdapter DT = new SqlDataAdapter(Properties.Settings.Default.RemoteSQL, LCN))
+                {
+                    DataTable TB = new DataTable();
+                    DT.Fill(TB);
+                    dgRemote.DataSource = TB;
+                }
+            }
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable TBLocal = new DataTable();
+            DataTable TBRemote = new DataTable();
+            
+            using (SqlConnection LCN = new SqlConnection(Properties.Settings.Default.LocalCN))
+            {
+                LCN.Open();
+                int DaysBefore = Properties.Settings.Default.DaysBefore;
+                string ShopName = Properties.Settings.Default.ShopName;
+                string ShopID = Properties.Settings.Default.ShopID;
+                string DateField = Properties.Settings.Default.DateField;
+                string Cdate = UT.FN_FormatDate(DateTime.Now.AddDays(DaysBefore * -1), true, true);
+                
+
+                using (SqlDataAdapter DT = new SqlDataAdapter(Properties.Settings.Default.LocalSQL + " Where " + DateField + ">'" + Cdate + "'", LCN))
+                {
+                    
+                    DT.Fill(ds);
+                   
+
+                }
+            }
+            using (SqlConnection LCN = new SqlConnection(Properties.Settings.Default.RemoteCN))
+            {
+                LCN.Open();
+                using (SqlDataAdapter DT = new SqlDataAdapter(Properties.Settings.Default.RemoteSQL, LCN))
+                {
+                    
+                    DT.Fill(TBRemote);
+                 
+                }
+            }
+            TBLocal = ds.Tables[0];
+            //var query =from u in TBLocal.AsEnumera;
+            BindingSource bc = new BindingSource();
+            bc.DataSource = TBLocal;
+            dgRemote.DataSource = bc;
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(UT.FN_FormatDate(DateTime.Now, false, true));
+        }
+    }
+
+
+}
